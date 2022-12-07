531f2c69c7d61b5a6f8ab20abb2b1850ae4807b6
Wisdom <adeniyi.wisdom@gmail.com>
2022-12-07 09:41:25 +0100

    updating working directory

---
+++ backend/router/userRouter.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const controller = require("../../backend/controller/userController");
+
+//router
+    //.get('/', controller.getAllUser);
+
+
+Module.exports =router;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
+const bodyParse = require("body-parser");
 // const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
 const port = process.env.PORT || 5000;
@@ -9,5 +10,9 @@ connectDB();
 const app = express();
 
 app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.sent("Welcome to zuri class");
+});
 
 app.listen(port, () => console.log(`starting server at port ${port}`));
